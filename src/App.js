563374bf90726319368b2c3ec3a0c4653425a065
unknown <thota.eswar@infovision.com>
2021-11-15 09:39:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import  restaurantItems  from './data';
+//import download from './images'
 
 function App() {
+
+  const [restoDetails, setrestoDetails] = useState([]);
+
+useEffect(()=>{
+  setrestoDetails(restaurantItems.category)
+  console.log(restoDetails,"detai;s")
+},[restoDetails])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor:'#808080' }}>
+       {/* <h1>{restaurantItems.restaurant_name}</h1> */}
+       <img src={require("./images/download3.jpg").default} alt="download" style={{ height:30,width:'90%',marginBottom:12 }}/>
+    {
+      restoDetails.map((items)=>{
+        return (
+          <div  style={{ marginLeft:'15px',marginRight:'15px',backgroundColor:'white',borderRadius:10 }}>  
+                  
+                 <div style={{ display:'flex',paddingLeft:12 }}> 
+                 <h3 style={{ textAlign:'left' }}>{items.category_name}</h3>
+                 <div style={{ height:12,width:12,backgroundColor:items.color,marginTop:26,marginLeft:20 }}/>
+                 </div>
+                 {
+                   items.food_types.map(foodTypes=>{
+                   
+                   return (
+                           <div style={{ marginBottom:12,padding:12 }}>
+                           <div style={{ display:'flex',justifyContent:'space-between',fontSize:14 }}> 
+                              <div>{foodTypes.item_name}</div>
+                              <div>{foodTypes.price}</div>
+                           </div>
+                           <div style={{ textAlign:'left',fontSize:12,color:'#C8C8C8' }}>{foodTypes.description}</div>
+                           <div style={{ borderTop: "2px solid #F5F5F5", marginTop:2}}></div>
+                           </div> 
+                   )})
+                 }
+          </div>     
+      )})
+    }
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,143 @@
+
+   const restaurantItems = {
+        "restaurant_id": 1001,
+        "restaurant_name": "paradise",
+        "category": [
+          
+            {
+              "categeory_id": 1,
+              "category_name":"veg_starters",
+              "color": "green",
+              "food_types": [
+              {
+                "veg_starters_id": 1,
+                "item_name": "Paneer Manchurian Dry",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "veg_starters_id": 2,
+                "item_name": "Paneer lahori",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "veg_starters_id": 3,
+                "item_name": "Paneer tikka",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "veg_starters_id": 4,
+                "item_name": "Paneer 65",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "veg_starters_id": 5,
+                "item_name": "Paneer chill",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "veg_starters_id": 6,
+                "item_name": "kadai chill",
+                "price": 300,
+                "description": "Chicken Biriyani with Dum"
+              },
+            ]
+          },
+          {
+            "categeory_id": 2,
+            "category_name":"non_veg_starters",
+            "color": "red",
+            "food_types":[
+              {
+                "non_veg_starters_id": 1,
+                "item_name": "Chicken 65",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "non_veg_starters_id": 2,
+                "item_name": "Chicken 65",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "non_veg_starters_id": 3,
+                "item_name": "Chicken Manchurian",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "non_veg_starters_id": 4,
+                "item_name": "Chicken Lollypop",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "non_veg_starters_id": 5,
+                "item_name": "Ginger Chicken",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "non_veg_starters_id": 6,
+                "item_name": "Chicken Chilli Chicken",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              }
+            ]
+          },
+          {
+            "categeory_id": 3,
+            "category_name":"biriyani",
+            "color": "red",
+            "food_types":[
+              {
+                "item_id": 1,
+                "item_name": "Chicken biriyani",
+                "price": 260,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "item_id": 2,
+                "item_name": "Chicken biriyani full",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "item_id": 3,
+                "item_name": "Chicken Biriyani Special",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "item_id": 4,
+                "item_name": "Mutton Biriyani",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "item_id": 5,
+                "item_name": "Mutton biriyani full",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              },
+              {
+                "item_id": 6,
+                "item_name": "Mutton biriyani Special",
+                "price": 280,
+                "description": "Chicken Biriyani with Dum"
+              }
+            ]
+          }
+
+        ]
+      }
+
+      export default restaurantItems;
+
+
+     
